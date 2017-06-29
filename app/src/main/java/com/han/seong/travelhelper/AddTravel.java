a3ec3144ecfbe0e5c82b103ea1721beacd229235
Seong Hyun Han <seonghyunhan7193@gmail.com>
2017-06-29 10:33:21 +0900

    Add Layout on AddTravel

--- app/src/main/java/com/han/seong/travelhelper/AddTravel.java
+++ app/src/main/java/com/han/seong/travelhelper/AddTravel.java
@@ -3,8 +3,8 @@ package com.han.seong.travelhelper;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-
-import com.han.seong.travelhelper.R;
+import android.widget.TabHost;
+import android.widget.TabHost.TabSpec;
 
 public class AddTravel extends AppCompatActivity {
 
@@ -12,6 +12,35 @@ public class AddTravel extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_travel);
+
+        setUpTabContent();
+
+
+    }
+
+
+    public void setUpTabContent(){
+        TabHost tabHost = (TabHost)findViewById(R.id.tabHost);
+        tabHost.setup();
+
+        TabSpec spec1 = tabHost.newTabSpec("Tab1").setContent(R.id.tab1).setIndicator(getString(R.string.tab1));
+        tabHost.addTab(spec1);
+        TabSpec spec2 = tabHost.newTabSpec("Tab1").setContent(R.id.tab2).setIndicator(getString(R.string.tab2));
+        tabHost.addTab(spec2);
+        TabSpec spec3 = tabHost.newTabSpec("Tab1").setContent(R.id.tab3).setIndicator(getString(R.string.tab3));
+        tabHost.addTab(spec3);
+        TabSpec spec4 = tabHost.newTabSpec("Tab1").setContent(R.id.tab4).setIndicator(getString(R.string.tab4));
+        tabHost.addTab(spec4);
+        TabSpec spec5 = tabHost.newTabSpec("Tab1").setContent(R.id.tab5).setIndicator(getString(R.string.tab5));
+        tabHost.addTab(spec5);
+
+        tabHost.getTabWidget().getChildAt(0).getLayoutParams().height=80;
+        tabHost.getTabWidget().getChildAt(1).getLayoutParams().height=80;
+        tabHost.getTabWidget().getChildAt(2).getLayoutParams().height=80;
+        tabHost.getTabWidget().getChildAt(3).getLayoutParams().height=80;
+        tabHost.getTabWidget().getChildAt(4).getLayoutParams().height=80;
+
+
     }
 
 }
