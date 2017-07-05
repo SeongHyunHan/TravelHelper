86538d0b1a06e30e2c629d6bef036be7375c1377
Seong Hyun Han <seonghyunhan7193@gmail.com>
2017-07-05 17:38:14 +0900

    Worked on AddFinance - create xml (ing 5)

--- app/src/main/java/com/han/seong/travelhelper/AddTravel.java
+++ app/src/main/java/com/han/seong/travelhelper/AddTravel.java
@@ -1,6 +1,5 @@
 package com.han.seong.travelhelper;
 
-
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
--- app/src/main/java/com/han/seong/travelhelper/travelDetail/AddFinance.java
+++ app/src/main/java/com/han/seong/travelhelper/travelDetail/AddFinance.java
@@ -1,8 +1,17 @@
 package com.han.seong.travelhelper.travelDetail;
 
+import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import com.han.seong.travelhelper.R;
 
-
+import io.realm.Realm;
 
 public class AddFinance extends AppCompatActivity{
+    private Realm mRealm;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_finance);
+    }
 }
